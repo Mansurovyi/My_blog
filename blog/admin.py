edc32826f80c22f0886f2e49db2f41e09f835f31
Mansur <akmetov_m@iuca.kg>
2023-09-25 12:56:02 +0600

    Post detail, post edit

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['category', ...]
